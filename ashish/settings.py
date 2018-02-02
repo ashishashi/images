0263aa05d28b78c88358fb96d3d29ae49eeaa830
apoorvkhare07 <apoorvkhare007@gmail.com>
2018-02-02 17:42:46 +0000

    Ho gaya

--- ashish/settings.py
+++ ashish/settings.py
@@ -120,4 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join("/home/gen/images",'media')
 MEDIA_URL = '/media/'
